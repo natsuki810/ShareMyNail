82cbce78c8fa3448af53e69c791a3ac6c35616f0
EC2 Default User <ec2-user@ip-172-31-15-97.ap-northeast-1.compute.internal>
2024-03-31 12:37:22 +0000

    返信ほ非同期化

--- app/controllers/public/replies_controller.rb
+++ app/controllers/public/replies_controller.rb
@@ -3,14 +3,18 @@ class Public::RepliesController < ApplicationController
 
   def create
     @comment = Comment.find(params[:comment_id])
-    @reply = Reply.new(reply_params)
-    @reply.user_id = current_user.id
-    if @reply.save
+    reply = Reply.new(reply_params)
+    reply.user_id = current_user.id
+    if reply.save
       flash[:notice_comment] = "投稿に成功しました"
-      redirect_to designs_show_path(@comment.design.id)
+      @design = @comment.design
+      reply_count = 0
+      @design.comments.each do |comment|
+        reply_count = reply_count + comment.replies.count
+      end
+      @total_comment = reply_count + @design.comments.count
     else
       flash[:notice_comment] = "投稿に失敗しました"
-      redirect_to designs_show_path(@comment.design.id)
     end
   end
 
@@ -18,28 +22,43 @@ class Public::RepliesController < ApplicationController
     @reply = Reply.find(params[:id])
     @comment = Comment.find(params[:comment_id])
     if @reply.update(reply_params)
-      flash[:notice_comment] = "投稿に成功しました"
-      redirect_to designs_show_path(@comment.design.id)
+      flash[:notice_comment] = "更新に成功しました"
+      # redirect_to designs_show_path(@comment.design.id)
+      @design = @comment.design
+      reply_count = 0
+      @design.comments.each do |comment|
+        reply_count = reply_count + comment.replies.count
+      end
+      @total_comment = reply_count + @design.comments.count
+
     else
-      flash[:notice_comment] = "投稿に失敗しました"
-      redirect_to designs_show_path(@comment.design.id)
+      flash[:notice_comment] = "更新に失敗しました"
     end
   end
 
   def destroy
     @reply = Reply.find(params[:id])
     @comment = Comment.find(params[:comment_id])
-    @reply.destroy
-    redirect_to designs_show_path(@comment.design.id)
+    if @reply.destroy
+      flash[:notice_comment] = "削除しました"
+      @design = @comment.design
+      reply_count = 0
+      @design.comments.each do |comment|
+        reply_count = reply_count + comment.replies.count
+      end
+      @total_comment = reply_count + @design.comments.count
+    else
+      flash[:notice_comment] = "削除に失敗しました"
+    end
   end
 
   def replies_update
     @reply = Reply.find(params[:id])
     if @reply.update(reply_params)
-      flash[:notice_comment] = "投稿に成功しました"
+      flash[:notice_comment] = "更新に成功しました"
       redirect_to users_comments_path
     else
-      flash[:notice_comment] = "投稿に失敗しました"
+      flash[:notice_comment] = "更新に失敗しました"
       redirect_to users_comments_path
     end
   end
